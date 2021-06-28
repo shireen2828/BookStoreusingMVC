2c101909ce5d445297916a51e5b38ac762199ea1
unknown <shireenkk28@gmail.com>
2021-06-28 21:23:27 +0530

    [shireen]added the creation of project

---
+++ BookstoreProject/Models/Register.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookstoreProject.Models
+{
+    public class Register
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
