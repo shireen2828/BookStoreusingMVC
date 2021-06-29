0101d279e1370b6d40f1e67b5ebf3a46a1dde969
unknown <shireenkk28@gmail.com>
2021-06-29 22:06:10 +0530

    [shireen]added the controller and view for UserRegister

--- BookstoreProject/Controllers/UserController.cs
+++ BookstoreProject/Controllers/UserController.cs
@@ -1,17 +1,42 @@
-﻿using System;
+﻿using ManagerLayer.Interfaces;
+using ModelsLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace BookstoreProject.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUserManager userManager;
+
+        public UserController(IUserManager userManager)
+        {
+            this.userManager = userManager;
+        }
         // GET: User
         public ActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterModel register)
+        {
+            try
+            {
+                var Result = this.userManager.RegisterUser(register);
+                ViewBag.Message = "User registered successfully";
+                // return View();
+                return RedirectToAction("Login");
+            }
+            catch (Exception)
+            {
+                return ViewBag.Message = "User registration unsuccessfull";
+            }
         }
     }
 }
